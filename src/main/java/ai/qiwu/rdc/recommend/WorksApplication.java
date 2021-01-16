00fb6e72f92c40340e9d76dd55cce47fffffc6a2
xunguoke <1638049431@qq.com>
2021-01-16 16:08:55 +0800

    v1.3

--- src/main/java/ai/qiwu/rdc/recommend/WorksApplication.java
+++ src/main/java/ai/qiwu/rdc/recommend/WorksApplication.java
@@ -8,11 +8,10 @@ import tk.mybatis.spring.annotation.MapperScan;
 /**
  * 手表推荐作品启动类
  * @author hjd
- *
  */
 @SpringBootApplication
 @EnableScheduling
-@MapperScan("ai.qiwu.com.cn.dao")
+@MapperScan("ai.qiwu.rdc.recommend.dao")
 public class WorksApplication {
     public static void main(String[] args) {
         SpringApplication.run(WorksApplication.class,args);
